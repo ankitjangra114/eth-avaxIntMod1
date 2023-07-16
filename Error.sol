3cc13eac42291a583bb031ca6bb096bb30d82d63
Ankit Jangra <135188111+ankitjangra114@users.noreply.github.com>
2023-07-16 16:00:40 +0530

    Add files via upload

---
+++ Error.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract ErrorHandle {
+   
+    function Assert(uint a) public pure{
+        assert(a!=0);
+    }
+
+    function divide(uint num1, uint num2) public pure returns (uint){
+        if(num2==0){
+           
+            revert("num2 should not be zero");
+            
+        }
+        return num1/num2;
+    }
+    uint x=5;
+    function divide(uint y) public view returns (uint){
+        require(y!=0,"if value of y is zero ,then it show error");
+        return x/y;
+
+    }
+
+}
